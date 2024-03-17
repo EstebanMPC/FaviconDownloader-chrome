782626702c969ec6a4f9360efb0890ccfc3ef070
estebanmpc <estebanmpc@icloud.com>
2024-03-17 01:01:48 -0400

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,29 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const onClick = async () => {
-    let[tab] = await chrome.tabs.query({ active: true })
-    chrome.scripting.executeScript({
-      target: {tabId : tab.id!},
-      func: () => {
-        alert('Hello hello, Hola');
+    // let[tab] = await chrome.tabs.query({ active: true })
+    // chrome.scripting.executeScript({
+    //   target: {tabId : tab.id!},
+    //   func: () => {
+    //     // alert('Hello hello, Hola');
+    //   }
+    // });
+    chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+      function(tabs){
+          alert(`http://www.google.com/s2/favicons?domain=${tabs[0].url}`);
       }
-    });
+    );
   }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Your Favicon Collection</h1>
-      <div className="card">
-        <button onClick={onClick}>
-          Click me
-        </button>
-        <div>
-          <div>
-            <img src="https://assets.algoexpert.io/static/images/main-favicon.png"></img>
-            <h3>algoexpert</h3>
-          </div>
-          <div>
-            <img src="https://assets.algoexpert.io/static/images/main-favicon.png"></img>
-            <h3>algoexpert</h3>
-          </div>
-          <div>
-            <img src="https://assets.algoexpert.io/static/images/main-favicon.png"></img>
-            <h3>algoexpert</h3>
-          </div>
-        </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="container">
+      <h1>😎</h1>
+      <h3>FaviconDownloader</h3>
+      <button onClick={onClick}>
+          Download Favicon
+      </button>
+    </div>
   )
 }
 
